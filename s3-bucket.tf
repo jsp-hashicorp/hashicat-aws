ad940d3e2440acfc62030d91509cb38acc99574f
JS Park <jsp@hashicorp.com>
2021-06-21 13:33:03 +0900

    Added s3 bucket module
    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
-#module "s3-bucket" {
-  #source  = "app.terraform.io/HC_JSP_TF_Webinar/s3-bucket/aws"
- # version = "1.15.0"
+module "s3-bucket" {
+  source  = "app.terraform.io/jsp-training/s3-bucket/aws"
+  version = "1.16.0"
   # insert required variables here
-#  bucket_prefix = "jsp"
-#}
+ bucket_prefix = "jsp"
+}
